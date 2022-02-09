af3a556703bc2df31dea80fd81d2a12113915101
MrUnluckyy <Justas.Sobutas@gmail.com>
2022-02-09 16:56:44 +0200

    Break homepage into smaller components

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import Directory from "../../components/Directory/Directory";
+
+import "./homepage.scss";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
